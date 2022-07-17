8fc547cbcc4bb108e0395a941352e6bce67278b2
root <root@discslab-server2.CS.McGill.CA>
2022-07-17 12:30:46 -0400

    New install scripts for discslab DGX1

--- setupvm.sh
+++ setupvm.sh
@@ -7,23 +7,42 @@ then
         exit -1
 fi
 
+# Update packages
+apt update -y
+apt upgrade -y
+apt autoremove -y
+
+# Install bpftrace and dependencies
+# Install mpstat
+# Install pip3
+# Install docker (don't install the snap version, it's a different package)
+apt-get install -y bpftrace sysstat python3-pip docker.io
+
+
 # clone all mlcommons (maybe done in a previous step)
 # Cp the workload we care about (image_segmentation)
 git clone https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/training.git /mlcommons_training
 
+pushd /mlcommons_training
+git remote set-url origin https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/training.git
+popd
+
 # Had a problem with runtime=nvidia, went back and tried to install cuda docker (didnt have to do this before but there was a recent update that might have changed that)
 /mlcommons_training/install_cuda_docker.sh 
 
-
 # Install tracing tools
 git clone https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/mlperf-traces.git /tracing_tools
-cd /tracing_tools
-# Checkout correct branch for experiments
-# Mods done: disk name is xvda vs sda
-git checkout awsvm
+pushd /tracing_tools
+# Set the remote url with the token 
+git remote set-url origin https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/mlperf-traces.git
+popd
+
 
 # Install experiment scheduling tools
 git clone https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/experiments.git /experiments
+pushd /experiments
+git remote set-url origin https://jovonho:${GIT_API_TOKEN}@github.com/jovonho/experiments.git
+popd
 
 
 #Clone Imseg raw data
